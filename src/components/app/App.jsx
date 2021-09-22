763941c86cfa830d86325c81a7c34d724bc851d0
Brianna Bias <brianna.bias@gmail.com>
2021-09-22 16:35:54 -0700

    init and app

---
+++ src/components/app/App.jsx
@@ -0,0 +1,18 @@
+const useRecord = (init) => {
+  // TODO: Implement me!!
+};
+
+function App() {
+  const { current, undo, redo, record } = useRecord('#FF0000');
+
+  return (
+    <>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input type="color" value={current} onChange={({ target }) => record(target.value)} />
+      <div style={{ backgroundColor: current, width: '10rem', height: '10rem' }}></div>
+    </>
+  )
+}
+
+export default App;
