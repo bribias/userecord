5f9996a86d99c2614f5ba74223e4806173f43fc3
Brianna Bias <brianna.bias@gmail.com>
2021-09-22 16:40:57 -0700

    added useRecord inital

--- src/custom-hooks/useRecord.js
+++ src/custom-hooks/useRecord.js
@@ -2,5 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const useRecord = (initialColor) => {
-    
+    const [current, useCurrent] = useState(initialColor);
+    const [record, setRecord] = useState();
+    const undo = () => { };
+    const redo = () => { };
+
+    useEffect(() => { }, []);
+
+    return { current, undo, redo, record };
 };
